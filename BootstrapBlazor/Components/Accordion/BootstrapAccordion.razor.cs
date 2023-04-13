664541d860b86ed4ba534cda1ca2f54624cb5226
gaofen13 <gaofen13@163.com>
2023-04-13 16:29:40 +0800

    Add Accordion component

---
+++ BootstrapBlazor/Components/Accordion/BootstrapAccordion.razor.cs
@@ -0,0 +1,50 @@
+﻿using BootstrapBlazor.Utilities;
+using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor
+{
+    public partial class BootstrapAccordion : BootstrapComponentBase
+    {
+        private readonly List<BootstrapAccordionItem> _items = new();
+
+        private string Classname =>
+          new ClassBuilder("accordion")
+            .AddClass("accordion-flush", Flush)
+            .AddClass(Class)
+            .Build();
+
+        [Parameter]
+        public bool Flush { get; set; }
+
+        [Parameter]
+        public bool MultiOpen { get; set; }
+
+        public void AddAccordionItem(BootstrapAccordionItem item)
+        {
+            if (!_items.Contains(item))
+            {
+                _items.Add(item);
+            }
+        }
+
+        public void RemoveAccordionItem(BootstrapAccordionItem item)
+        {
+            if (_items.Contains(item))
+            {
+                _items.Remove(item);
+            }
+        }
+
+        public void ToggleItem(BootstrapAccordionItem item)
+        {
+            if (!MultiOpen & !item.Open)
+            {
+                foreach (var openedItem in _items.Where(i => i.Open))
+                {
+                    openedItem.Toggle();
+                }
+            }
+            item.Toggle();
+        }
+    }
+}
---
+++ BootstrapBlazor/Components/Accordion/BootstrapAccordionItem.razor.cs
@@ -0,0 +1,61 @@
+﻿using BootstrapBlazor.Utilities;
+using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor
+{
+    public partial class BootstrapAccordionItem : BootstrapComponentBase, IDisposable
+    {
+        private string Classname =>
+          new ClassBuilder("accordion-item")
+            .AddClass(Class)
+            .Build();
+
+        private string HeaderClassname =>
+            new ClassBuilder("accordion-button")
+            .AddClass("collapsed", !Open)
+            .Build();
+
+        private string BodyClassname =>
+            new ClassBuilder("accordion-collapse collapse")
+            .AddClass("show", Open)
+            .Build();
+
+        private string BodyStylelist =>
+            new StyleBuilder("transition", "opacity 0.35s ease")
+            .AddStyle("opacity", Open ? "1" : "0")
+            .AddStyle("height", "0", !Open)
+            .AddStyle("display", "block")
+            .Build();
+
+        [CascadingParameter]
+        private BootstrapAccordion? Accordion { get; set; }
+
+        [Parameter]
+        public string? Title { get; set; }
+
+        [Parameter]
+        public bool Open { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> OpenChanged { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Accordion?.AddAccordionItem(this);
+            base.OnInitialized();
+        }
+
+        public void Toggle()
+        {
+            Open = !Open;
+            _ = OpenChanged.InvokeAsync(Open);
+            StateHasChanged();
+        }
+
+        void IDisposable.Dispose()
+        {
+            Accordion?.RemoveAccordionItem(this);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
