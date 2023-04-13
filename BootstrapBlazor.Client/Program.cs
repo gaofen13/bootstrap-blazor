66a1bad0c837fd7daa6f24cc50bfcc1bfb47ef0d
gaofen13 <gaofen13@163.com>
2023-04-13 09:08:56 +0800

    fix wasm

--- BootstrapBlazor.Client/Program.cs
+++ BootstrapBlazor.Client/Program.cs
@@ -1,11 +1,13 @@
 using BootstrapBlazor;
 using BootstrapBlazor.Shared;
+using BootstrapBlazor.Shared.Data;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddSingleton<WeatherForecastService>();
 
 builder.Services.AddBootstrapBlazor();
 
