a786c9cee22679aaa2f31e4b7dd1b183f4180f5b
gaofen13 <gaofen13@163.com>
2023-04-18 15:25:28 +0800

    add listgroup component

---
+++ BootstrapBlazor/Components/ListGroup/BootstrapListGroup.razor.cs
@@ -0,0 +1,42 @@
+﻿using BootstrapBlazor.Utilities;
+using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor
+{
+    public partial class BootstrapListGroup : BootstrapComponentBase
+    {
+        private string Classname =>
+          new ClassBuilder("list-group")
+            .AddClass("list-group-numbered", Numbered)
+            .AddClass($"list-group-horizontal{(HorizontalBreakpoint == null ? "" : $"-{HorizontalBreakpoint}")}", Horizontal)
+            .AddClass("list-group-flush", Flush)
+            .AddClass(Class)
+            .Build();
+
+        private string HtmlTag { get; set; } = "ul";
+
+        [Parameter]
+        public bool Actionable { get; set; }
+
+        [Parameter]
+        public bool Numbered { get; set; }
+
+        [Parameter]
+        public bool Horizontal { get; set; }
+
+        [Parameter]
+        public Size? HorizontalBreakpoint { get; set; }
+
+        [Parameter]
+        public bool Flush { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (Actionable)
+            {
+                HtmlTag = "div";
+            }
+            base.OnInitialized();
+        }
+    }
+}
---
+++ BootstrapBlazor/Components/ListGroup/BootstrapListGroupItem.razor.cs
@@ -0,0 +1,56 @@
+﻿using BootstrapBlazor.Utilities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazor
+{
+    public partial class BootstrapListGroupItem : BootstrapComponentBase
+    {
+        private string Classname =>
+          new ClassBuilder("list-group-item")
+            .AddClass($"list-group-item-{Color}", Color != null)
+            .AddClass("list-group-item-action", Href != null || ListGroup?.Actionable == true)
+            .AddClass("disabled", Disabled)
+            .AddClass("active", Active)
+            .AddClass(Class)
+            .Build();
+
+        private string HtmlTag { get; set; } = "li";
+
+        [CascadingParameter]
+        private BootstrapListGroup? ListGroup { get; set; }
+
+        [Parameter]
+        public bool Disabled { get; set; }
+
+        [Parameter]
+        public bool Active { get; set; }
+
+        [Parameter]
+        public string? Href { get; set; }
+
+        [Parameter]
+        public Color? Color { get; set; }
+
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (Href != null)
+            {
+                HtmlTag = "a";
+            }
+            else if (ListGroup?.Actionable == true)
+            {
+                HtmlTag = "button";
+            }
+            base.OnInitialized();
+        }
+    }
+}
