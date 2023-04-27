aecc299a0b63e4856674a909cb15194dd121b4f0
gaofen13 <gaofen13@163.com>
2023-04-27 14:36:42 +0800

    add navbar dropdown

---
+++ BootstrapBlazor/Components/Navbar/BootstrapNavDropdown.razor.cs
@@ -0,0 +1,73 @@
+﻿using BootstrapBlazor.Utilities;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazor
+{
+    public partial class BootstrapNavDropdown : BootstrapComponentBase
+    {
+        private string Classname =>
+            new ClassBuilder("nav-item dropdown")
+            .AddClass(Class)
+            .Build();
+
+        private string MenuClassname =>
+            new ClassBuilder("dropdown-menu")
+            .AddClass("show", Show)
+            .AddClass($"text-{TextAlignment}")
+            .AddClass($"{ItemAlignment}-0", ItemAlignment != HorizontalPosition.center)
+            .AddClass("start-50 translate-middle-x", ItemAlignment == HorizontalPosition.center)
+            .AddClass($"dropdown-menu-xs-{BreakpointXsAlignment}", BreakpointXsAlignment != null)
+            .AddClass($"dropdown-menu-sm-{BreakpointSmAlignment}", BreakpointSmAlignment != null)
+            .AddClass($"dropdown-menu-md-{BreakpointMdAlignment}", BreakpointMdAlignment != null)
+            .AddClass($"dropdown-menu-lg-{BreakpointLgAlignment}", BreakpointLgAlignment != null)
+            .AddClass($"dropdown-menu-xl-{BreakpointXlAlignment}", BreakpointXlAlignment != null)
+            .AddClass($"dropdown-menu-xxl-{BreakpointXxlAlignment}", BreakpointXxlAlignment != null)
+            .Build();
+
+        [Parameter]
+        public bool Show { get; set; }
+
+        [Parameter]
+        public string? Title { get; set; }
+
+        [Parameter]
+        public string? HeaderText { get; set; }
+
+        [Parameter]
+        public RenderFragment? MenuContent { get; set; }
+
+        [Parameter]
+        public HorizontalPosition ItemAlignment { get; set; } = HorizontalPosition.start;
+
+        [Parameter]
+        public HorizontalPosition TextAlignment { get; set; } = HorizontalPosition.start;
+
+        [Parameter]
+        public HorizontalPosition? BreakpointXsAlignment { get; set; }
+
+        [Parameter]
+        public HorizontalPosition? BreakpointSmAlignment { get; set; }
+
+        [Parameter]
+        public HorizontalPosition? BreakpointMdAlignment { get; set; }
+
+        [Parameter]
+        public HorizontalPosition? BreakpointLgAlignment { get; set; }
+
+        [Parameter]
+        public HorizontalPosition? BreakpointXlAlignment { get; set; }
+
+        [Parameter]
+        public HorizontalPosition? BreakpointXxlAlignment { get; set; }
+
+        private void OnclickActivator()
+        {
+            Show = !Show;
+        }
+    }
+}
