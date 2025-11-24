aeb6a1ebe90ab3d668dac20f598455e53a09bd18
gaofen13 <gaofen13@163.com>
2025-11-24 09:05:36 +0800

    fix webapp rendermode-auto

--- BootstrapBlazor.Client/Program.cs
+++ BootstrapBlazor.Client/Program.cs
@@ -1,5 +1,10 @@
+using BootstrapBlazor;
+using BootstrapBlazor.Shared.Data;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddBootstrapBlazor();
 
 await builder.Build().RunAsync();
